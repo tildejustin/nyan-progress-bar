45a5c7db184408becc1e49a1a1d8b92bcc1efcd7
Hiroshi Miura <miurahr@linux.com>
2023-12-17 11:48:21 +0900

    fix: replace removed SwingUtilities2

--- src/main/java/tokyo/northside/intellij/plugins/NyanProgressBarUi.java
+++ src/main/java/tokyo/northside/intellij/plugins/NyanProgressBarUi.java
@@ -7,12 +7,12 @@ import com.intellij.ui.JBColor;
 import com.intellij.util.ui.GraphicsUtil;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
-import sun.swing.SwingUtilities2;
 
 import javax.swing.Icon;
 import javax.swing.JComponent;
 import javax.swing.SwingConstants;
 import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.basic.BasicGraphicsUtils;
 import javax.swing.plaf.basic.BasicProgressBarUI;
 import java.awt.Color;
 import java.awt.Container;
@@ -255,11 +255,11 @@ public class NyanProgressBarUi extends BasicProgressBarUI {
 
         if (progressBar.getOrientation() == SwingConstants.HORIZONTAL) {
             g2.setColor(getSelectionBackground());
-            SwingUtilities2.drawString(progressBar, g2, progressString,
+            BasicGraphicsUtils.drawString(progressBar, g2, progressString,
                     renderLocation.x, renderLocation.y);
             g2.setColor(getSelectionForeground());
             g2.clipRect(fillStart, y, amountFull, h);
-            SwingUtilities2.drawString(progressBar, g2, progressString,
+            BasicGraphicsUtils.drawString(progressBar, g2, progressString,
                     renderLocation.x, renderLocation.y);
         } else { // VERTICAL
             g2.setColor(getSelectionBackground());
@@ -268,11 +268,11 @@ public class NyanProgressBarUi extends BasicProgressBarUI {
             g2.setFont(progressBar.getFont().deriveFont(rotate));
             renderLocation = getStringPlacement(g2, progressString,
                     x, y, w, h);
-            SwingUtilities2.drawString(progressBar, g2, progressString,
+            BasicGraphicsUtils.drawString(progressBar, g2, progressString,
                     renderLocation.x, renderLocation.y);
             g2.setColor(getSelectionForeground());
             g2.clipRect(x, fillStart, w, amountFull);
-            SwingUtilities2.drawString(progressBar, g2, progressString,
+            BasicGraphicsUtils.drawString(progressBar, g2, progressString,
                     renderLocation.x, renderLocation.y);
         }
         g2.setClip(oldClip);
